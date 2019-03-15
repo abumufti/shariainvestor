a4c46f5bb6adfe94e3ba75df855c36494985a100
Isa Anshori <isa.anshori.skom@gmail.com>
2019-03-15 21:48:08 +0700

    tambahkan iklan

--- views/site/blog.php
+++ views/site/blog.php
@@ -21,7 +21,12 @@ $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
     <p>Sakinah Berinvestasi Saham.</p><br><br>
 </div>
 
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+<!-- Container (About Section) -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-9 " style="text-align:justify;">
+          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- Homepage Leaderboard -->
 <ins class="adsbygoogle"
 accesskey=""style="display:inline-block;width:728px;height:90px"
@@ -29,12 +34,7 @@ data-ad-client="ca-pub-1234567890123456"
 cite=""data-ad-slot="1234567890"></ins>
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
-</script> 
-
-<!-- Container (About Section) -->
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-sm-9 " style="text-align:justify;">
+</script>   
           <?php foreach($posts as $index => $value){ ?>
             <div class="card" id="myList">
                 <div class="card-body">
