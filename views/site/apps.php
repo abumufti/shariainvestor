a7d883abf49d6f40a6fd1aa1832f9ff502adb884
Isa Anshori <isa.anshori.skom@gmail.com>
2019-03-15 23:46:29 +0700

    tambahkan iklan

--- views/site/apps.php
+++ views/site/apps.php
@@ -11,7 +11,6 @@ $this->title = "Apps";
         <div class="panel-heading text-center">Stock Calculator</div>
         <form class="form-horizontal" action="#" id="StockCalculator">
             <div class="panel-body">
-                <p>Kalkulator ini membantu kamu menentukan target profit, sell, dan lot.</p>
                 <div class="form-group">
                     <label for="type">Type:</label>
                     <select class="form-control" id="stockcalculator-type" >
