d618b4e658ddd096eb7c29fdaa69b117d628ff50
Isa Anshori <isa.anshori.skom@gmail.com>
2019-03-15 22:32:16 +0700

    tambahkan iklan

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -249,6 +249,25 @@ use yii\helpers\BaseUrl;
         font-size: 150px;
     }
   }
+  /* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
   </style>
 </head>
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
