07a20839caf4e73ffe7206656737aec7a8342f18
Isa Anshori <isa.anshori.skom@gmail.com>
2019-03-15 22:25:31 +0700

    tambahkan iklan

--- views/site/issi.php
+++ views/site/issi.php
@@ -18,11 +18,18 @@ foreach($index as $i => $value){
 }
 
 ?>
-<div class="jumbotron text-justify" style="background-image: url('<?=Yii::$app->homeUrl;?>img/home-bg.jpg'); background-size:cover;margin-bottom:0;">
-  <h2><?= $preface->title; ?></h2> 
-  <?= $preface->body; ?>
+<div class="jumbotron text-center" style="background-image: url('<?=Yii::$app->homeUrl;?>img/home-bg.jpg'); background-size:cover;margin-bottom:0">
+  <h1>Sharia Investor</h1> 
+  <p>Sakinah Berinvestasi Saham.</p><br><br>
 </div>
-<br>
+
+<div class="container-fluid bg-grey" style="text-align:justify;margin-bottom:10px;">
+           
+ 
+            <h3><?= $preface->title; ?></h3>
+             <?= $preface->body; ?>
+            
+</div>
 
 <div class="container-fluid" style="text-align:justify;">
     
