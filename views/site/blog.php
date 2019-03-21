ff5c4e3be4ffdebbab1ae2327660ad1cd7db0bb4
Isa Anshori <isa.anshori.skom@gmail.com>
2019-03-22 06:36:02 +0700

    default search

--- views/site/blog.php
+++ views/site/blog.php
@@ -84,18 +84,7 @@ $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
                 <div class="panel panel-default" style="margin-top:10px;">
                     <div class="panel-heading">Search</div>
                     <div class="panel-body">
-                        <script>
-  (function() {
-    var cx = 'partner-pub-3744700612400365:6807239632';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
-</script>
-<gcse:searchbox-only></gcse:searchbox-only>
+                        <input type="text" id="myInput" class="form-control" placeholder="Search for...">
                     </div>
                 </div>
         
