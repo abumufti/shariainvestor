0fb07397c8d97271df87332432cacbc836800a95
Isa Anshori <isa.anshori.skom@gmail.com>
2019-03-23 22:42:39 +0700

    perbaikan title

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,6 @@ use yii\helpers\BaseStringHelper;
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="<?= strip_tags(BaseStringHelper::explode(Yii::$app->view->params['description'],'</p>')[0].'</p>'); ?>" />
   <meta name="image_src" content="https://shariainvestor.com/img/display.png">
-
   <!-- FB -->
   <meta property="og:title" content="<?= $this->title; ?>" />
   <meta property="og:url" content="<?= (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>" />
@@ -68,7 +67,7 @@ use yii\helpers\BaseStringHelper;
     
   <style>
     body {
-        font: 400 15px Lato, sans-serif;
+        font: 400 18px Lato, sans-serif;
         line-height: 1.8;
         color: #303030;
     }
